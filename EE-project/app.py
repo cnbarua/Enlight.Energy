908ce99bcfefa33da65a229a91bb017cb4c9eecd
Claudio Barua <cbarua@hotmail.com>
2024-04-13 19:56:27 -0400

    Update 04/13/24

--- EE-project/app.py
+++ EE-project/app.py
@@ -1,7 +1,18 @@
 from flask import Flask, render_template, request, session, redirect, url_for
+from flask_mysqldb import MySQL
+import config
+import hashlib
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '39bb37cf36d3b29a9280d8a70a0eed42'
+
+app.config['SECRET_KEY'] = config.HEX_SEC_KEY
+app.config['MYSQL_HOST'] = config.MYSQL_HOST
+app.config['MYSQL_USER'] = config.MYSQL_USER
+app.config['MYSQL_PASSWORD'] = config.MYSQL_PASSWORD
+app.config['MYSQL_DB'] = config.MYSQL_DB
+app.config['MYSQL_PORT'] = config.MYSQL_PORT
+
+mysql = MySQL(app)
 
 @app.route('/', methods=['GET'])
 def home():
@@ -10,12 +21,17 @@ def home():
 @app.route("/login", methods=['POST'])
 def login():
     email = request.form['email']
-    passw = request.form['pass']
+    passw = hashlib.md5(request.form['pass'].encode()).hexdigest()
 
-    if email == 'cbarua@hotmail.com' and passw == "Asdf1234":
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM users WHERE email = %s AND password = %s", (email, passw))
+    user = cur.fetchone();
+    cur.close()
+
+    if user is not None:
         session['email'] = email
-        session['name'] = 'Claudio'
-        session['surname'] = 'Barua'
+        session['name'] = user[1]
+        session['surname'] = user[2]
 
         return redirect(url_for('tasks'))
     else:
@@ -29,6 +45,12 @@ def tasks():
 def logout():
     session.clear()
     return redirect(url_for('home'))
+
+@app.route('/new-task', methods=['POST'])
+def newTask():
+    title = request.form['title']
+    descri = request.form['description']
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
